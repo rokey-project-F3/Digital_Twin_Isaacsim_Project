c70728dc26a6eb2b4207992f54a8925d8d531c80
gabmin <rlarkqals@gmail.com>
2025-11-19 19:59:09 +0900

    cube attach 로직 오류 수정

--- cube_attach_to_robot.py
+++ cube_attach_to_robot.py
@@ -1,11 +1,15 @@
 import omni.usd
-from pxr import UsdGeom, Sdf, Gf, Usd
+from pxr import UsdGeom, Sdf, Gf, Usd, UsdPhysics
 import omni.kit.app
+from omni.isaac.dynamic_control import _dynamic_control
+from sensor_msgs.msg import JointState
 
+dc = _dynamic_control.acquire_dynamic_control_interface()
 stage = omni.usd.get_context().get_stage()
 
-CUBE_PATH = "/World/PROJECT_1/Cube"
-BASE_LINK_PATH = "/World/PROJECT_1/Nova_Carter_ROS/chassis_link"
+
+CUBE_PATH = "/World/Cube"
+BASE_LINK_PATH = "/World/Nova_Carter_ROS/chassis_link/base_link"
 
 print("Cube:", CUBE_PATH)
 print("Base:", BASE_LINK_PATH)
@@ -26,25 +30,35 @@ def on_update(dt):
     robot_pos = get_world_position(BASE_LINK_PATH)
     cube_pos  = get_world_position(CUBE_PATH)
 
+
     if robot_pos is None or cube_pos is None:
         return
 
     dist = (cube_pos - robot_pos).GetLength()
-    # print("dist:", dist)
+    print("dist:", dist, attached)
 
-    if (not attached) and dist < 0.3:
+    if (not attached) and dist < 1:
         print("📦 Attaching cube!")
 
         old = Sdf.Path(CUBE_PATH)
-        new = Sdf.Path(BASE_LINK_PATH).AppendChild("Cube")
+        new = Sdf.Path(BASE_LINK_PATH)
 
-        ok = stage.MovePrim(old, new)
+        print(old, new)
 
-        if ok:
-            print("✅ Attached!")
-            attached = True
-        else:
-            print("❌ MovePrim failed")
+        # joint_props = _dynamic_control.D6JointProperties()
+        # joint_props.name = "robot_cube_joint"
+        # joint_props.body0 = old   # 부모 prim
+        # joint_props.body1 = new  # 자식 prim
+
+        
+        joint_prim = UsdPhysics.FixedJoint.Define(stage, '/World/Cube/Joints')
+        joint_prim.CreateBody0Rel().SetTargets([new])
+        joint_prim.CreateBody1Rel().SetTargets([old])
+
+        joint_prim.CreateLocalPos0Attr().Set(Gf.Vec3f(0.0, 0.0, 0.6))
+
+        attached = True
+
 
 
 subscription = omni.kit.app.get_app().get_update_event_stream().create_subscription_to_pop(on_update)
